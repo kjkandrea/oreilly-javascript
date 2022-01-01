0f762d4b06e3afb4b9586d9ed80ed6fc669a6d13
jongkeun-kim <kjkandrea@gmail.com>
2022-01-01 15:54:07 +0900

    Add string is immutable

--- tutorial/stringIsImmutable.js
+++ tutorial/stringIsImmutable.js
@@ -1,8 +1,16 @@
 let abc = 'abc'
 
 console.log(abc[1])
-abc[1] = '👽'
+abc[1] = 'z' // No. You Can't
 console.log(abc[1])
 
-console.log('abc' === abc)
+console.log('abc' !== abc) // false
 
+String.prototype.replaceAt = function (index, replacement) {
+  return this.substr(0, index)
+    + replacement
+    + this.substr(index + replacement.length)
+}
+
+abc = abc.replaceAt(1, 'z'); // string is immutable. but you can
+console.log('abc' !== abc) // true
