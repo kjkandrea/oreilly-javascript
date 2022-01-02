02bb7dc6dbe4d2934bf4441805131e034ed9cf33
jongkeun-kim <kjkandrea@gmail.com>
2022-01-02 15:05:34 +0900

    Add bitwise tutorial

---
+++ 00_Bit/bitwise.js
@@ -0,0 +1,6 @@
+let bit = (1).toString(2)
+console.log(bit) // 2진법 : 1
+
+bit = bit << 1
+console.log(bit) // 2진법 규칙에 맞게 1을 이동시킴. 십진법으로 출력 : 2
+console.log(bit.toString(2)) // 2진법 규칙에 맞게 1을 이동시킴. 이진법으로 출력 : 10
