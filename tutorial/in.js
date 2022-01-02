c8bc1d40369a852b44963638ef62d331886c0829
jongkeun-kim <kjkandrea@gmail.com>
2022-01-02 15:41:33 +0900

    Add in operator

--- tutorial/in.js
+++ tutorial/in.js
@@ -2,7 +2,7 @@ const array = [1, 2, 3]
 console.log(0 in array); // 해당 key 가 있는지 검사할 수 있음.
 console.log(2 in array);
 console.log(3 in array);
-console.log(array.hasOwnProperty(2)) // 이거랑 동일
+console.log(array.hasOwnProperty(2)) // 이거는 prototype chain 에 영향 안 받음
 
 const object = {
   x: 1,
@@ -12,4 +12,6 @@ const object = {
 console.log('x' in object); // 해당 key 가 있는지 검사할 수 있음.
 console.log('z' in object);
 console.log('a' in object);
-console.log(object.hasOwnProperty('z')) // 이거랑 동일
+console.log(object.hasOwnProperty('z')) // 이거는 prototype chain 에 영향 안 받음
+
+// 결론 : hasOwnProperty 쓰자
