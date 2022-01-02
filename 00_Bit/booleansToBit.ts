e93bf5122ef232523730fa09bea0ae49b294c840
jongkeun-kim <kjkandrea@gmail.com>
2022-01-02 15:25:32 +0900

    Add Booleans isEqual

--- 00_Bit/booleansToBit.ts
+++ 00_Bit/booleansToBit.ts
@@ -1,3 +1,4 @@
+// boolean list 를 이진수로 다루는 자료형
 class Booleans {
 	private readonly _bit: string;
 
@@ -12,7 +13,18 @@ class Booleans {
 	toBit(booleans: boolean[]): string {
 		return booleans.map(bool => bool ? 1 : 0).join('')
 	}
+
+	isEqual(bit: string): boolean {
+		return this._bit === bit;
+	}
 }
 
 const { bit } = new Booleans([true, true, false, true])
 console.log(bit)
+
+// boolean list 와 boolean list 를 최소비용으로 비교한다.
+const foo = new Booleans([true, true, false, true])
+const bar = new Booleans([true, true, false, true])
+console.log(foo.isEqual(bar.bit))
+
+console.log(foo.isEqual('1100'))
